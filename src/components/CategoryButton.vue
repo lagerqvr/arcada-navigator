483f37a0022330f7b93ecd1d1e9be86f38622f56
obcido <jonathan.harkonen@gmail.com>
2021-09-22 08:43:09 +0300

    category button component

---
+++ src/components/CategoryButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="background" :style="{'background-color': color}">
+    <font-awesome-icon :icon="icon" size="3x" />
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'CategoryButton',
+    props: {
+        name: String,
+        icon: String,
+        color: {
+          type: String,
+        },
+        route: {
+            type: String,
+            default: '.'
+        }
+    },
+    data() {
+      return {
+      }
+    }
+}
+</script>
+
+<style scoped>
+  .background {
+    padding: 50px 10px;
+    margin: 7px;
+    flex: 1 0 20%;
+    border-radius: 30px;
+    background-color: rgba(red, green, blue, alpha);
+  }
+  p {
+    font-size: 1.3rem;
+  }
+</style>
