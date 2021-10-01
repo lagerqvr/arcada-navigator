2a36c859607e9a5faea14db3011e07122f1b00a2
Ixurano <tommy_tommy95@hotmail.com>
2021-10-01 16:51:07 +0300

    hsl API fungerar

--- src/views/Hsl.vue
+++ src/views/Hsl.vue
@@ -1,65 +1,99 @@
 <template>
-  <div>
-    <div v-for="post in posts" v-bind:key="post.id">
-      <h2>{{ post.bus }}</h2>
-      <p>{{ post.time }}</p>
+  <div class="timeTabel-container">
+    <div v-for="post in posts" v-bind:key=" post.$.line + post.$.time ">
+      <table class="flex-container">
+        <p class="destination">{{ post.$.destination }}</p>
+        <p class="line">{{ post.$.line }}</p>
+        <p class="time">{{ formateTime(post.$.time) }}</p>
+        <p class="type">{{ post.$.type }}</p>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import { parseString } from "xml2js";
 
 export default {
   name: "home",
   data: () => ({
     posts: [],
+    polling: null,
+    
   }),
-
   created() {
     this.fetch();
+    this.pollData();
   },
+    beforeDestroy() {
+      clearInterval(this.polling);
+    },
   methods: {
     fetch() {
       axios
-        .get(
-          "https://famnen.arcada.fi/arbs/infotv/hst.php",
-          function (req, res) {
-            res.headers({ "Access-Control-Allow-Origin": "http://localhost:8080/" }),
-            res.mode("cors");
-          }
-        )
+        .get("https://famnen.arcada.fi/arbs/infotv/hst.php")
         .then((response) => {
-          this.posts = response.data.data;
+          var xmlData = response.data;
+          parseString(xmlData, (err, results) => {
+            this.posts = results.hst.departure;
+            console.log("hello polling");
+          });
         });
+    },
+    formateTime(time) {
+      var currentTime = new Date();
+      var arrivalTime = new Date(time);
+      var remainingTime = arrivalTime - currentTime;
+      var remainingMinutes = Math.round(remainingTime / 60000);
+
+      return remainingMinutes;
+    },
+
+    pollData() {
+      this.polling = setInterval(() => {
+        this.fetch();
+      }, 1000);
     },
   },
 };
-
-/* export default {
-  data() {
-    return {
-      posts: [],
-    };
-  },
-
- methods: {
-    async getData() {
-      try {
-        const response = await this.$http.get(
-            "https://famnen.arcada.fi/arbs/infotv/hst.php"
-            //"http://jsonplaceholder.typicode.com/posts"
-        );
-        // JSON responses are automatically parsed.
-        this.posts = response.data;
-      } catch (error) {
-        console.log(error);
-      }
-    },
-  },
-
-  created() {
-    this.getData();
-  },
-}; */
 </script>
+<style scoped>
+.timeTabel-container {
+  max-width: 600px;
+  overflow: hidden;
+  display: flex;
+  max-height: 250px;
+  flex-wrap: wrap;
+  flex-direction: column;
+}
+
+.flex-container {
+  justify-content: space-between;
+  width: 300px;
+  height: 25px;
+  margin: 0px;
+  display: flex;
+  border: solid 2px;
+  background-color: lightblue;
+}
+p {
+  text-align: left;
+  padding: 0px;
+  margin: auto 0;
+  display: block;
+}
+.destination {
+  padding-left: 4px;
+  width: 40%;
+}
+.line {
+  width: 10%;
+}
+.time {
+  width: 15%;
+}
+.type {
+  width: 15%;
+}
+</style>
