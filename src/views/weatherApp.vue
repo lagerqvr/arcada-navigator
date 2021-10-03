8d5ac70231a043be4c6a02e1832fae452843477b
Ixurano <tommy_tommy95@hotmail.com>
2021-10-03 13:55:45 +0300

    weatherApp påbörjad api tillagd med token.

---
+++ src/views/weatherApp.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="weather-container">
+    <div class="current-weather">
+      <p>hej</p>
+    </div>
+    <!-- end current weather -->
+  </div>
+  <!-- weather container end -->
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      weather: {
+        cityName: "Helsinki",
+        country: "Finland",
+        temperature: "",
+        humidity: "",
+        lowTemp: "",
+        highTemp: "",
+      },
+    };
+  },
+  created() {
+    this.getWeather();
+  },
+
+  methods: {
+    getWeather: async function () {
+      const apiKey = "a4061165de10b2d44ee5832fd238a336";
+      const baseURL = `api.openweathermap.org/data/2.5/weather?q=helsinki&appid=${apiKey}&units=metric`;
+      const response = await fetch(baseURL);
+      const data = await response.json();
+      console.log(data);
+    },
+  },
+};
+</script>
+<style>
+</style>
