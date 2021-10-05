4e5474d7c09191cb3cf75255fbfea2f8b913803d
Rasmus Lagerqvist <rasmus.o.lagerqvist@gmail.com>
2021-10-05 12:16:00 +0300

    Fixed visual bugs caused by BootstrapVue

--- src/views/CategoryMenu.vue
+++ src/views/CategoryMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Visar border bara på welcome screen -->
-  <div class="border" :class="showWelcome ? 'active' : ''">
+  <div class="border-container" :class="showWelcome ? 'active' : ''">
     <img
       id="logo"
       @click="showWelcome = true"
@@ -65,13 +65,13 @@ export default {
   width: 90%;
   margin: auto;
 }
-.border {
+.border-container{
   border-radius: 30px;
   padding-bottom: 5%;
   margin: 5% 5%;
   transition: 1.5s;
 }
-.border.active {
+.border-container.active {
   border: 2px solid black;
   margin: 4.91% 12%;
 }
